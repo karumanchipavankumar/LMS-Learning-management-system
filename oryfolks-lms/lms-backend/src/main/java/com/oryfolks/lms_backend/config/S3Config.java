478aaf645c07ef8c6afeab3f3788aed57c6c06f8
Thanmai <thanmai@example.com>
2026-08-07 14:59:14 +0530

    Migrated from s3 to minio

--- oryfolks-lms/lms-backend/src/main/java/com/oryfolks/lms_backend/config/S3Config.java
+++ oryfolks-lms/lms-backend/src/main/java/com/oryfolks/lms_backend/config/S3Config.java
@@ -2,6 +2,7 @@ package com.oryfolks.lms_backend.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,15 +21,30 @@ public class S3Config {
     @Value("${aws.region}")
     private String region;
 
+    @Value("${aws.s3.endpoint}")
+    private String endpoint;
+
     @Bean
     public AmazonS3 amazonS3() {
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(credentials))
-                .build();
+        if (endpoint == null || endpoint.trim().isEmpty()) {
+            // Default AWS S3 Client Configuration
+            return AmazonS3ClientBuilder.standard()
+                    .withRegion(region)
+                    .withCredentials(
+                            new AWSStaticCredentialsProvider(credentials))
+                    .build();
+        } else {
+            // MinIO (or custom S3 compatible storage) Client Configuration
+            return AmazonS3ClientBuilder.standard()
+                    .withEndpointConfiguration(
+                            new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                    .withCredentials(
+                            new AWSStaticCredentialsProvider(credentials))
+                    .withPathStyleAccessEnabled(true)
+                    .build();
+        }
     }
 
 }
